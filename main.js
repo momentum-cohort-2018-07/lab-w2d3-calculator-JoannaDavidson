4779aba7755f3fde1b5028e5f0a6f467e8629d08
Joanna Davidson <joanna@shrikesong.com>
2018-08-08 17:27:21 -0400

    Save point. Figuring out the js more.
    Trying to avoid having an event for every single button.
    Think I have how I will do that in mind.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var 
+var inputButton = document.querySelectorAll('calc-button');
+
+
+
   var displayDiv = document.getElementByID('calc-display');
   if (displayDiv.innerText === '') {
     displayDiv.innerText = numberPress;
   } else {
   displayDiv.innerText += numberPress;
+
+  var displayDiv = document.getElementByID('calc-display');
+  if (displayDiv.innerText === '') {
+    displayDiv.innerText = numberPress;
+  } else {
+  displayDiv.innerText += numberPress;
+
+
